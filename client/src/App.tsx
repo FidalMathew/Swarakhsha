73cb443be51a3f9f21686b6749256ac78f7dade3
Fidal Mathew <fidal15perfect@gmail.com>
2025-09-27 10:29:17 +0530

    added context and wallet connect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
+import { SwarProvider } from "./context/SwarProvider";
+import ConnectWallet from "./pages/ConnectWallet";
 import Home from "./pages/Home";
 import ReportDetail from "./pages/ReportDetail";
 import Reports from "./pages/Reports";
@@ -9,17 +11,20 @@ import Testify from "./pages/Testify";
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/searchlocation" element={<SearchLocation />} />
-          <Route path="/reports" element={<Reports />} />
-          <Route path="/testify" element={<Testify />} />
-          <Route path="/report-detail/:id" element={<ReportDetail />} />
-          {/* Add other routes as needed */}
-          <Route path="*" element={<div>404 Not Found</div>} />
-        </Routes>
-      </BrowserRouter>
+      <SwarProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/connect" element={<ConnectWallet />} />
+            <Route path="/searchlocation" element={<SearchLocation />} />
+            <Route path="/reports" element={<Reports />} />
+            <Route path="/testify" element={<Testify />} />
+            <Route path="/report-detail/:id" element={<ReportDetail />} />
+            {/* Add other routes as needed */}
+            <Route path="*" element={<div>404 Not Found</div>} />
+          </Routes>
+        </BrowserRouter>
+      </SwarProvider>
     </>
   );
 }
